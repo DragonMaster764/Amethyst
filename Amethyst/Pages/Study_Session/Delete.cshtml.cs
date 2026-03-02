a885bb4c24d3619cd8eb24d5bc7984c7f6c5456f
DragonMaster764 <68863979+DragonMaster764@users.noreply.github.com>
2026-03-02 17:53:00 -0500

    Added Study_Session CRUD pages

---
+++ Amethyst/Pages/Study_Session/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Amethyst.Data;
+using Amethyst.Models;
+
+namespace Amethyst.Pages.Study_Session
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Amethyst.Data.ApplicationDbContext _context;
+
+        public DeleteModel(Amethyst.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public StudySession StudySession { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studysession = await _context.StudySessions.FirstOrDefaultAsync(m => m.SessionId == id);
+
+            if (studysession == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                StudySession = studysession;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studysession = await _context.StudySessions.FindAsync(id);
+            if (studysession != null)
+            {
+                StudySession = studysession;
+                _context.StudySessions.Remove(StudySession);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Amethyst/Pages/Study_Session/Edit.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Amethyst.Data;
+using Amethyst.Models;
+
+namespace Amethyst.Pages.Study_Session
+{
+    public class EditModel : PageModel
+    {
+        private readonly Amethyst.Data.ApplicationDbContext _context;
+
+        public EditModel(Amethyst.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public StudySession StudySession { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studysession =  await _context.StudySessions.FirstOrDefaultAsync(m => m.SessionId == id);
+            if (studysession == null)
+            {
+                return NotFound();
+            }
+            StudySession = studysession;
+           ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "ProfileId");
+           ViewData["ProfileId"] = new SelectList(_context.Set<Profile>(), "ProfileId", "ProfileId");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(StudySession).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudySessionExists(StudySession.SessionId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool StudySessionExists(int id)
+        {
+            return _context.StudySessions.Any(e => e.SessionId == id);
+        }
+    }
+}
