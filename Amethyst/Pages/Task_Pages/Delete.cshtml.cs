1fb2ba77943777ada99c660b0c49971efd2accd1
DragonMaster764 <68863979+DragonMaster764@users.noreply.github.com>
2026-03-02 17:56:38 -0500

    Added Task CRUD pages

---
+++ Amethyst/Pages/Task_Pages/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Amethyst.Data;
+using Amethyst.Models;
+
+namespace Amethyst.Pages.Task_Pages
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Amethyst.Data.ApplicationDbContext _context;
+
+        public DeleteModel(Amethyst.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Tasks Tasks { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tasks = await _context.Tasks.FirstOrDefaultAsync(m => m.TaskId == id);
+
+            if (tasks == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Tasks = tasks;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tasks = await _context.Tasks.FindAsync(id);
+            if (tasks != null)
+            {
+                Tasks = tasks;
+                _context.Tasks.Remove(Tasks);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Amethyst/Pages/Task_Pages/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Amethyst.Data;
+using Amethyst.Models;
+
+namespace Amethyst.Pages.Task_Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Amethyst.Data.ApplicationDbContext _context;
+
+        public DetailsModel(Amethyst.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Tasks Tasks { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tasks = await _context.Tasks.FirstOrDefaultAsync(m => m.TaskId == id);
+            if (tasks == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Tasks = tasks;
+            }
+            return Page();
+        }
+    }
+}
