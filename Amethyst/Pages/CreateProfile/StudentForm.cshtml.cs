09e5f5ea348e60cf9d113422b7307659f2901043
DANIE2705 <dlebron@jacksonville.edu>
2026-03-12 00:38:13 -0400

    Student Form and regular User Form started

--- Amethyst/Pages/CreateProfile/StudentForm.cshtml.cs
+++ Amethyst/Pages/CreateProfile/StudentForm.cshtml.cs
@@ -1,12 +1,40 @@
+using Amethyst.Data;
+using Amethyst.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace Amethyst.Pages.CreateProfile
 {
-    public class IndexModel : PageModel
+    public class StudentFormModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
+        public StudentFormModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Profile NewProfile { get; set; } = new Profile();
+
         public void OnGet()
         {
         }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            NewProfile.ProfileId = Guid.NewGuid().ToString();
+            NewProfile.UserCreationDate = DateTime.UtcNow;
+
+            _context.Profiles.Add(NewProfile);
+            _context.SaveChanges();
+
+            return RedirectToPage("/Success");
+        }
     }
 }
+
--- Amethyst/Pages/CreateProfile/UserForm.cshtml.cs
+++ Amethyst/Pages/CreateProfile/UserForm.cshtml.cs
@@ -1,12 +1,43 @@
+using Amethyst.Data;
+using Amethyst.Models; // your Profile model namespace
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace Amethyst.Pages.CreateProfile
 {
-    public class IndexModel : PageModel
+    public class UserFormModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
+        public UserFormModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Profile NewProfile { get; set; } = new Profile();
+
         public void OnGet()
         {
         }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            // Generate a GUID for profile_id
+            NewProfile.ProfileId = Guid.NewGuid().ToString();
+
+            // Set creation date
+            NewProfile.UserCreationDate = DateTime.UtcNow;
+
+            _context.Profiles.Add(NewProfile);
+            _context.SaveChanges();
+
+            return RedirectToPage("/Success");
+        }
     }
 }
+
