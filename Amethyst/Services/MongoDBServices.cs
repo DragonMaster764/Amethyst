861f8ec383e24148d6ef98ca79b92a1e7905733e
DragonMaster764 <68863979+DragonMaster764@users.noreply.github.com>
2026-03-26 23:10:06 -0400

    Added the MongoDBServices class.

---
+++ Amethyst/Services/MongoDBServices.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+
+namespace Amethyst.Services
+{
+    public class MongoDBServices
+    {
+        private readonly IMongoCollection<Study_Songs> _collection;
+
+        public MongoDBServices(IConfiguration configuration)
+        {
+            MongoClient client = new MongoClient(configuration["MongoDBSettings:ConnectionString"]);
+            IMongoDatabase database = client.GetDatabase(configuration["MongoDBSettings:DatabaseName"]);
+            _collection = database.GetCollection<Study_Songs>(configuration["MongoDBSettings:CollectionName"]);
+        }
+
+        public async Task<List<Study_Songs>> GetAllSongsAsync()
+        {
+            return await _collection.Find(_ => true).ToListAsync();
+        }
+    }
+}
