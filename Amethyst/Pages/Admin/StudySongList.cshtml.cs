fb935475b6559c163ee74a9c96ff40668ef6942f
DragonMaster764 <68863979+DragonMaster764@users.noreply.github.com>
2026-03-26 23:17:01 -0400

    Added CRUD methods to the MongoDB Service, and an empty Razor page

---
+++ Amethyst/Pages/Admin/StudySongList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Amethyst.Pages.Admin
+{
+    public class StudySongListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Amethyst/Services/MongoDBServices.cs
+++ Amethyst/Services/MongoDBServices.cs
@@ -13,9 +13,19 @@ namespace Amethyst.Services
             _collection = database.GetCollection<Study_Songs>(configuration["MongoDBSettings:CollectionName"]);
         }
 
-        public async Task<List<Study_Songs>> GetAllSongsAsync()
-        {
-            return await _collection.Find(_ => true).ToListAsync();
-        }
+        public async Task<List<Study_Songs>> GetAllAsync() =>
+             await _collection.Find(_ => true).ToListAsync();
+
+        public async Task<Study_Songs> GetAsync(string id) =>
+            await _collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Study_Songs song) =>
+            await _collection.InsertOneAsync(song);
+
+        public async Task UpdateAsync(string id, Study_Songs song) =>
+            await _collection.ReplaceOneAsync(x => x.Id == id, song);
+
+        public async Task DeleteAsync(string id) =>
+            await _collection.DeleteOneAsync(x => x.Id == id);
     }
 }
