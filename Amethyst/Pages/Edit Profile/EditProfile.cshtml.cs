38f07198ff1e96fd5097ca384bccea1260205f68
DANIE2705 <dlebron@jacksonville.edu>
2026-04-07 23:15:20 -0400

    Edit Profile created

---
+++ Amethyst/Pages/Edit Profile/EditProfile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Amethyst.Pages.Edit_Profile
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
