0e8ebf57898d7ab8bf6f44f10795daf888e6d685
DANIE2705 <dlebron@jacksonville.edu>
2026-04-14 00:35:21 -0400

    Adding the Edit Profile logic

---
+++ Amethyst/Pages/EditProfile/OtherPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Amethyst.Pages.EditProfile
+{
+    public class OtherPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
